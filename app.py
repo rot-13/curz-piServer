507bbbf11bdeff9ea7260c62f2607a89777b9041
Oded Argon <oargon@paypal.com>
2016-06-26 16:36:07 +0300

    keep things alive

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
 import pychromecast as pycc
 from websocket import create_connection
 
@@ -18,10 +22,19 @@ def guess_content_type(extension):
     return 'audio/mp4'
 
 
+def ping(ws):
+    while ws.connected:
+        gevent.sleep(5)
+        ws.send('ping')
+
+
 def run():
     ws = create_connection("wss://%s/connect" % (stream_server_ip))
+    gevent.spawn(ping, ws)
     while ws.connected:
         file_url = ws.recv()
+        if file_url == 'ping':
+            continue
         extension = file_url.split('.')[-1]
         cast = pycc.get_chromecast(friendly_name="FloydCast")
         cast.media_controller.play_media(file_url, guess_content_type(extension))
