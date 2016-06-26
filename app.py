d3189582a607886fae55e9d452c447bbf586262b
Oded Argon <oargon@paypal.com>
2016-06-26 20:24:22 +0300

    Add support for text to speach

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ monkey.patch_all()
 
 import pychromecast as pycc
 from websocket import create_connection
+import time
+from subprocess import call
 
 stream_server_ip = 'cpc-curz.herokuapp.com'
 stream_server_port = 443
@@ -21,6 +23,12 @@ def guess_content_type(extension):
     return 'audio/mp4'
 
 
+def generate_mp3(text, lang="en"):
+    filename = 'text_%s.mp3' % time.time()
+    call("espeak -v %s %s --stdout | avconv -i - -ar 44100 -ac 2 -ab 192k -f mp3 %s" % (lang, text, filename), shell=True)
+    return filename
+
+
 def ping(ws):
     while ws.connected:
         gevent.sleep(5)
@@ -32,13 +40,17 @@ def run():
     gevent.spawn(ping, ws)
     while ws.connected:
         gevent.sleep(0.1)
-        file_url = ws.recv()
-        if file_url == 'ping':
+        recv_string = ws.recv()
+        if recv_string == 'ping':
             continue
+        elif recv_string.startswith('text:'):
+            file_url = generate_mp3(recv_string[5:])
+        else:
+            file_url = recv_string
         extension = file_url.split('.')[-1]
         cast = pycc.get_chromecast(friendly_name="FloydCast")
         cast.media_controller.play_media(file_url, guess_content_type(extension))
-        print "got: " + file_url
+        print "playing: " + file_url
 
     ws.close()
 
